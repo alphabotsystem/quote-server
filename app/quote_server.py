c18969216e6d2e82df31010e75d27b85514db6c2
Matic Conradi <matic.conradi@me.com>
2022-01-02 21:44:22 +0100

    Cleanup

--- app/quote_server.py
+++ app/quote_server.py
@@ -120,6 +120,5 @@ class QuoteProcessor(object):
 
 if __name__ == "__main__":
 	environ["PRODUCTION_MODE"] = environ["PRODUCTION_MODE"] if "PRODUCTION_MODE" in environ and environ["PRODUCTION_MODE"] else ""
-	print("[Startup]: Quote Server is in startup, running in {} mode.".format("production" if environ["PRODUCTION_MODE"] else "development"))
 	quoteServer = QuoteProcessor()
 	quoteServer.run()
