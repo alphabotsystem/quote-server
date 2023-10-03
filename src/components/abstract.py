da7ac6c32b8f578b8ad35eb288f7066a013bdbd8
Matic Conradi <matic.conradi@me.com>
2023-10-03 19:20:35 +0200

    Bug fix

--- src/components/abstract.py
+++ src/components/abstract.py
@@ -178,7 +178,8 @@ class AbstractProvider(object):
 		plt.savefig(rawImageData, format="png", edgecolor="none")
 		plt.close(fig)
 		rawImageData.seek(0)
-		return Image.open(rawImageData)
+		# return Image.open(rawImageData)
+		return rawImageData
 
 	@v_args(inline=True)
 	class CalculateTree(Transformer):
